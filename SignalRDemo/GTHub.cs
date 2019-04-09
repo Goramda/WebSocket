3f8e1105b73147356b42bee90cdbb67034096fe3
nitipong <goddy_2556@hotmail.com>
2019-04-09 08:31:25 +0700

    First Commit

---
+++ SignalRDemo/GTHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.GT.SmartOffice.Models;
+
+namespace SignalRHub
+{
+    [HubName("GTHub")]
+    public class GTHub : Hub
+    {
+        public void DetermineLength(string message)
+        {
+            Console.WriteLine(message);
+
+            string newMessage = "response : "+message ;
+            Clients.All.ReceiveLength(newMessage);
+        }
+    }
+}
---
+++ SignalRDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://192.168.1.109:8080/";
+            
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine(string.Format("Server running at {0}", url));
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
